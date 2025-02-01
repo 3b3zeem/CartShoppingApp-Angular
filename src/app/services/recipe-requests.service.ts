8468dd668731611805bfc36797dc62498e8cb303
3b3zeem <ahmedzezo2247@gmail.com>
2025-02-01 16:27:58 +0200

    deployment

--- src/app/services/recipe-requests.service.ts
+++ src/app/services/recipe-requests.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment.development';
 
 @Injectable({
   providedIn: 'root'
@@ -10,10 +11,10 @@ export class RecipeRequestsService {
   constructor(private http: HttpClient) { }
 
   getProducts(skip: number = 0, limit: number = 10): Observable<any> {
-    return this.http.get(`https://dummyjson.com/products?limit=${limit}&skip=${skip}`);
+    return this.http.get(`${environment.baseURL}/products?limit=${limit}&skip=${skip}`);
   }
 
   getProductDetails(productId: number): Observable<any> {
-    return this.http.get(`https://dummyjson.com/products/${productId}`);
+    return this.http.get(`${environment.baseURL}/products/${productId}`);
   }
 }
